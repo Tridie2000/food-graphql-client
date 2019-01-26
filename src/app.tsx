adc0b83634f641d9d43cfb6e378e4c58537566e4
Kristof Van Miegem <kristof@codifly.be>
2019-01-26 13:53:37 +0100

    chore: initial project setup

---
+++ src/app.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div>
+        <h1>Hello everyone! This will become our foodies reservation webapp!</h1>
+        <br />
+        <br />
+        <img src="https://sayingimages.com/wp-content/uploads/im-on-a-seafood-diet-food-memes.jpg" />
+      </div>
+    );
+  }
+}
+
+export default App;
